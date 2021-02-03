4ef1b139e7d8c3e9e10576efff6f6338952fd229
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2021-02-02 21:34:23 -0800

    Update list.

--- scripts/update_godot_godex.sh
+++ scripts/update_godot_godex.sh
@@ -5,39 +5,14 @@ set -e
 git stash
 
 echo -e "Checkout remotes"
-git remote rm SaracenOne || true
-git remote add SaracenOne https://github.com/SaracenOne/godot
-git remote set-url --push SaracenOne https://example.com/
-git fetch SaracenOne
-git remote rm lyuma || true
-git remote add lyuma https://github.com/lyuma/godot
-git remote set-url --push lyuma https://example.com/
-git fetch lyuma
+git remote rm godot-ecs || true
+git remote add godot-ecs https://github.com/GodotECS/godot.git
+git remote set-url --push godot-ecs https://example.com/
+git fetch godot-ecs
 git remote rm godotengine || true
 git remote add godotengine https://github.com/godotengine/godot
 git remote set-url --push godotengine https://example.com/
 git fetch godotengine
-git remote rm v-sekai-godot || true
-git remote add v-sekai-godot https://github.com/V-Sekai/godot
-git remote set-url --push v-sekai-godot https://example.com/
-git fetch v-sekai-godot
-git remote rm "extended-fire-godot" || true
-git remote add "extended-fire-godot" git@github.com:godot-extended-libraries/godot-fire.git
-# Need
-# git remote set-url --push extended-fire-godot https://example.com/
-git fetch extended-fire-godot
-git remote rm fire || true
-git remote add fire git@github.com:fire/godot.git
-git remote set-url --push fire https://example.com/
-git fetch fire
-git remote rm RevoluPowered || true
-git remote add RevoluPowered https://github.com/RevoluPowered/godot.git
-git remote set-url --push RevoluPowered https://example.com/
-git fetch RevoluPowered
-git remote rm madmiraal || true
-git remote add madmiraal https://github.com/madmiraal/godot.git
-git remote set-url --push madmiraal https://example.com/
-git fetch madmiraal
 
 echo -e "Work"
 git checkout merge-script-master --force
