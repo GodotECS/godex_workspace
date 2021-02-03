97ab46a71fb224c57832f30ba54241a0d99dae16
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2021-02-02 21:36:10 -0800

    update scripts.

--- scripts/update_godot_godex.sh
+++ scripts/update_godot_godex.sh
@@ -6,7 +6,7 @@ git stash
 
 echo -e "Checkout remotes"
 git remote rm godot-ecs || true
-git remote add godot-ecs https://github.com/GodotECS/godot.git
+git remote add godot-ecs https://github.com/GodotECS/godot
 git remote set-url --push godot-ecs https://example.com/
 git fetch godot-ecs
 git remote rm godotengine || true
@@ -15,15 +15,17 @@ git remote set-url --push godotengine https://example.com/
 git fetch godotengine
 
 echo -e "Work"
-git checkout merge-script-master --force
-git branch -D "extended-fire" || true
-./thirdparty/git-assembler -av
-git checkout extended-fire -f
+export ORIGINAL_BRANCH=ecs-scripts-next
+export MERGE_REMOTE=godot-ecs
+export MERGE_BRANCH=ecs-main
+git checkout $ORIGINAL_BRANCH --force
+git branch -D $MERGE_BRANCH || true
+./thirdparty/git-assembler --assemble --verbose --recreate
+git checkout $MERGE_BRANCH -f
 export MERGE_DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
-export MERGE_TAG=$(echo extended-fire.$MERGE_DATE | tr ':' ' ' | tr -d ' \t\n\r')
-git merge -s ours remotes/extended-fire-godot/extended-fire -m "Commited at $MERGE_DATE."
+export MERGE_TAG=$(echo GodotECS.$MERGE_DATE | tr ':' ' ' | tr -d ' \t\n\r')
 git tag -a $MERGE_TAG -m "Commited at $MERGE_DATE."
-git push extended-fire-godot $MERGE_TAG
-git push extended-fire-godot extended-fire
-git checkout merge-script-master --force
-git branch -D extended-fire || true
+git push $MERGE_REMOTE $MERGE_TAG
+git push $MERGE_REMOTE $MERGE_BRANCH -f
+git checkout $ORIGINAL_BRANCH --force
+git branch -D $MERGE_BRANCH || true
